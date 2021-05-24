db6ad5fb6c16a7264495d13c709c5cc117d12c20
zuiluoxiaoyao <1214300029@qq.com>
2021-05-24 23:21:12 +0800

    提交信息

---
+++ servlet-02/src/main/java/com/kuang/servlet/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.kuang.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //this.getInitParameter()    初始化参数
+        //this.getServletConfig()    Servlet配置
+        //this.getServletContext()   Servlet上下文
+        ServletContext context = this.getServletContext();
+
+        String username = "秦疆";//数据
+        context.setAttribute("username",username);//将一个数据保存在了ServletContext中，名字为username，值为username
+
+
+
+
+        System.out.println("Hello");
+    }
+}
---
+++ servlet-02/src/main/java/com/kuang/servlet/ServletDemo03.java
@@ -0,0 +1,7 @@
+package com.kuang.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class ServletDemo03 extends HttpServlet {
+
+}
